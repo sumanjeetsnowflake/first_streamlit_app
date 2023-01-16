09566ef9c0047345547113239042fd8697429ce6
sumanjeetsnowflake <122826815+sumanjeetsnowflake@users.noreply.github.com>
2023-01-17 01:07:51 +0530

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title('My Parents new healthy diner')
+streamlit.title('My Parents new healthy  updted diner')
+
+streamlit.header('Menu for Breakfast')
+streamlit.text('Kadali Chhatuaa Chakadaa')
