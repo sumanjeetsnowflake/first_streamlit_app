0b17fb5c61e3564999cf25e8623a5589c7c2b7ee
sumanjeetsnowflake <122826815+sumanjeetsnowflake@users.noreply.github.com>
2023-01-25 00:58:50 +0530

    Update streamlit_app.py
    imported requests api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas
 import streamlit
+import requests
 streamlit.title('My Parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
@@ -14,3 +15,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
