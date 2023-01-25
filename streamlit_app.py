162800ec183d1ea3940e114197a020208d976645
sumanjeetsnowflake <122826815+sumanjeetsnowflake@users.noreply.github.com>
2023-01-25 17:21:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,7 @@ except URLError as e:
 #streamlit.dataframe(my_data_rows)
 
 streamlit.header("The fruit load list contains:")
+#snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
 	 my_cur.execute("select * from fruit_load_list")
