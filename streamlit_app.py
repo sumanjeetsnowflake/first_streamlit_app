28beb5c1b0f8e752585047d3f1dc54af1551d791
sumanjeetsnowflake <122826815+sumanjeetsnowflake@users.noreply.github.com>
2023-01-25 16:05:42 +0530

    Update streamlit_app.py
    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,5 +32,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
