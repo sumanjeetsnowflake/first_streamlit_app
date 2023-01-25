dd618f0c5175cfe8cd34c07038497cdb7ed50457
sumanjeetsnowflake <122826815+sumanjeetsnowflake@users.noreply.github.com>
2023-01-25 16:17:25 +0530

    Update streamlit_app.py
    scsca

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
