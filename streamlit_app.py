76eef36aa5deec4a6f9dfcf632a54faf1e5248da
sumanjeetsnowflake <122826815+sumanjeetsnowflake@users.noreply.github.com>
2023-01-25 18:00:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,7 @@ if streamlit.button('Get Fruit Load List'):
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-         #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    with my_cnx.cursor() as my_cur:         
 	 my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
          return  "Thanks for adding " + new_fruit
 		 
