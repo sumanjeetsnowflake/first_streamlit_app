7305d16e2da0047751e3fc7d9ff4ec3ce3ede0d8
sumanjeetsnowflake <122826815+sumanjeetsnowflake@users.noreply.github.com>
2023-01-25 17:15:33 +0530

    Update streamlit_app.py
    addded

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,8 +52,8 @@ def get_fruit_load_list():
 #add a button to load fruit list
 if streamlit.button('Get Fruit Load List'):
     my_cnx= snowflake.connector.connect(**streamlit.secrets["snowflake"])
-	my_data_rows = get_fruit_load_list()
-	streamit.dataframe(my_data_rows)
+    my_data_rows = get_fruit_load_list()
+    streamit.dataframe(my_data_rows)
 streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
